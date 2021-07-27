6e2f57fa264dddf7e7339140bb1c3a6663390d5a
Christopher D Couture <videogameroulette@yahoo.ca>
2021-07-27 06:20:36 -0400

    initial commit

---
+++ SRTDevilMayCry4DirectX/PluginConfiguration.cs
@@ -0,0 +1,38 @@
+﻿namespace SRTDevilMayCry4DirectX
+{
+    public class PluginConfiguration
+    {
+        public bool Debug { get; set; }
+        public bool ShowHPBars { get; set; }
+        public bool ShowIGT { get; set; }
+        public float ScalingFactor { get; set; }
+        public float PositionX { get; set; }
+        public float PositionY { get; set; }
+        public float EnemyHPPositionX { get; set; }
+        public float EnemyHPPositionY { get; set; }
+        public string IGTString { get; set; }
+        public string DevilTriggerString { get; set; }
+        public string StringFontName { get; set; }
+        public string EnemyString { get; set; }
+        public string VersionInfoString { get; set; }
+        public string GameInfoString { get; set; }
+
+        public PluginConfiguration()
+        {
+            Debug = true;
+            ShowHPBars = true;
+            ShowIGT = true;
+            ScalingFactor = 1f;
+            PositionX = 5f;
+            PositionY = 30f;
+            EnemyHPPositionX = -1;
+            EnemyHPPositionY = -1;
+            IGTString = "IGT:";
+            DevilTriggerString = "Devil Trigger:";
+            StringFontName = "Courier New";
+            EnemyString = "ENEMY HP";
+            VersionInfoString = "Version:";
+            GameInfoString = "Game Version:";
+        }
+    }
+}
---
+++ SRTDevilMayCry4DirectX/PluginInfo.cs
@@ -0,0 +1,26 @@
+﻿using SRTPluginBase;
+using System;
+
+namespace SRTDevilMayCry4DirectX
+{
+    internal class PluginInfo : IPluginInfo
+    {
+        public string Name => "DirectX Overlay UI (Devil May Cry 4 Special Edition)";
+
+        public string Description => "A DirectX-based Overlay User Interface for displaying Devil May Cry 4 Special Edition game memory values.";
+
+        public string Author => "VideoGameRoulette";
+
+        public Uri MoreInfoURL => new Uri("https://github.com/SpeedrunTooling/SRTDevilMayCry4DirectX");
+
+        public int VersionMajor => assemblyFileVersion.ProductMajorPart;
+
+        public int VersionMinor => assemblyFileVersion.ProductMinorPart;
+
+        public int VersionBuild => assemblyFileVersion.ProductBuildPart;
+
+        public int VersionRevision => assemblyFileVersion.ProductPrivatePart;
+
+        private System.Diagnostics.FileVersionInfo assemblyFileVersion = System.Diagnostics.FileVersionInfo.GetVersionInfo(System.Reflection.Assembly.GetExecutingAssembly().Location);
+    }
+}
